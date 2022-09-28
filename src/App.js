e4b2ad7889da9e32af693ea8adbc6044e6487b38
Estefania Ceballos <escebal2001@gmail.com>
2022-09-28 15:31:16 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Table,
+  Button,
+  Container,
+  Modal,
+  ModalHeader,
+  FormGroup,
+  ModalFooter,
+} from "reactstrap";
+import CharacterTable from "./CharacterTable";
+
+const data = [
+  { id: 1, nombre: "Iron Man", poder: "Inteligencia", estado: "Muerto" },
+  { id: 2, nombre: "Thor", poder: "Dios del trueno", estado: "Vivo" },
+  {
+    id: 3,
+    nombre: "Black Widow",
+    poder: "Destreza física",
+    estado: "Muerta",
+  },
+  { id: 4, nombre: "Capitan America", poder: "Fuerza", estado: "Muerto" },
+  { id: 5, personaje: "Hulk", poder: "Fuerza", estado: "Vivo" },
+  {
+    id: 6,
+    nombre: "Capitana Marvel",
+    poder: "Fuerza sobrehumana",
+    estado: "Viva",
+  },
+  { id: 7, nombre: "Spiderman", poder: "Sentido arácnido", estado: "Vivo" },
+  { id: 8, personaje: "Doctor Strange", poder: "Magia", estado: "Vivo" },
+  { id: 8, nombre: "Bruja Escarlata", poder: "Telequinisis", estado: "N/A" },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <br />
+        <Button color="primary">Insertar nuevo personaje </Button>
+        <br />
+        <br />
+
+        <Table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Personaje</th>
+              <th>Poder</th>
+              <th>Estado</th>
+            </tr>
+          </thead>
+          <CharacterTable />
+        </Table>
+      </Container>
+    </>
   );
 }
 
---
+++ src/CharacterTable.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const CharacterTable = () => {
+  return (
+    <tbody>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </tbody>
+  );
+};
+
+export default CharacterTable;
